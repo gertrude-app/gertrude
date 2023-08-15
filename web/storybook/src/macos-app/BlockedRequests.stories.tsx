gertrude-app/web

dash: added 'for parents' to logo in dashboard

storybook/src/macos-app/BlockedRequests.stories.tsx
@@ -33,6 +33,14 @@ export const LightMode = props({
   },
 });
 
+export const Wide = props({
+  ...LightMode.args,
+  width: 1200,
+  props: {
+    ...LightMode.args.props,
+  },
+});
+
 export const LightModeSelected = props({
   ...LightMode.args,
   props: {

gertrude-app/gertrude

web/storybook/src/macos-app/BlockedRequests.stories.tsx
@@ -24,6 +24,11 @@ export const Default = props({
   dispatch: () => {},
 });
 
+export const Wide = props({
+  ...Default.args,
+  width: 1200,
+});
+
 export const Selected = props({
   ...Default.args,
   unlockRequestExplanation: `need this for my math class, dad!`,