gertrude-app/swift

monorepo: prep for ios app api connection

api/Sources/Api/Models/Requests/SuspendFilterRequest.swift
@@ -2,42 +2,44 @@ import DuetSQL
 import Gertie
 import TaggedTime
 
-struct SuspendFilterRequest: Codable, Sendable, Equatable {
-  var id: Id
-  var computerUserId: UserDevice.Id
-  var status: RequestStatus
-  var scope: AppScope
-  var duration: Seconds<Int>
-  var requestComment: String?
-  var responseComment: String?
-  var extraMonitoring: String?
-  var createdAt = Date()
-  var updatedAt = Date()
+extension MacApp {
+  struct SuspendFilterRequest: Codable, Sendable, Equatable {
+    var id: Id
+    var computerUserId: UserDevice.Id
+    var status: RequestStatus
+    var scope: AppScope
+    var duration: Seconds<Int>
+    var requestComment: String?
+    var responseComment: String?
+    var extraMonitoring: String?
+    var createdAt = Date()
+    var updatedAt = Date()
 
-  init(
-    id: Id = .init(),
-    computerUserId: UserDevice.Id,
-    status: RequestStatus = .pending,
-    scope: AppScope,
-    duration: Seconds<Int> = 180,
-    requestComment: String? = nil,
-    responseComment: String? = nil,
-    extraMonitoring: String? = nil
-  ) {
-    self.id = id
-    self.computerUserId = computerUserId
-    self.status = status
-    self.scope = scope
-    self.duration = duration
-    self.requestComment = requestComment
-    self.responseComment = responseComment
-    self.extraMonitoring = extraMonitoring
+    init(
+      id: Id = .init(),
+      computerUserId: UserDevice.Id,
+      status: RequestStatus = .pending,
+      scope: AppScope,
+      duration: Seconds<Int> = 180,
+      requestComment: String? = nil,
+      responseComment: String? = nil,
+      extraMonitoring: String? = nil
+    ) {
+      self.id = id
+      self.computerUserId = computerUserId
+      self.status = status
+      self.scope = scope
+      self.duration = duration
+      self.requestComment = requestComment
+      self.responseComment = responseComment
+      self.extraMonitoring = extraMonitoring
+    }
   }
 }
 
 // loaders
 
-extension SuspendFilterRequest {
+extension MacApp.SuspendFilterRequest {
   func userDevice(in db: any DuetSQL.Client) async throws -> UserDevice {
     try await UserDevice.query()
       .where(.id == self.computerUserId)

api/Sources/Api/Services/AdminNotifications/AdminEvent.swift
@@ -20,12 +20,22 @@ enum AdminEvent: Equatable {
   }
 
   struct SuspendFilterRequestSubmitted: Equatable {
+    enum Context: Equatable {
+      case macapp(
+        computerUserId: UserDevice.Id,
+        requestId: MacApp.SuspendFilterRequest.Id
+      )
+      case iosapp(
+        deviceId: IOSApp.Device.Id,
+        requestId: IOSApp.SuspendFilterRequest.Id
+      )
+    }
+
     var dashboardUrl: String
-    var userDeviceId: UserDevice.Id
-    var userId: User.Id
-    var userName: String
+    var childId: User.Id
+    var childName: String
     var duration: Seconds<Int>
-    var requestId: SuspendFilterRequest.Id
     var requestComment: String?
+    var context: Context
   }
 }

gertrude-app/gertrude

swift/api/Sources/Api/Models/Requests/SuspendFilterRequest.swift
@@ -2,42 +2,44 @@ import DuetSQL
 import Gertie
 import TaggedTime
 
-struct SuspendFilterRequest: Codable, Sendable, Equatable {
-  var id: Id
-  var computerUserId: ComputerUser.Id
-  var status: RequestStatus
-  var scope: AppScope
-  var duration: Seconds<Int>
-  var requestComment: String?
-  var responseComment: String?
-  var extraMonitoring: String?
-  var createdAt = Date()
-  var updatedAt = Date()
+extension MacApp {
+  struct SuspendFilterRequest: Codable, Sendable, Equatable {
+    var id: Id
+    var computerUserId: ComputerUser.Id
+    var status: RequestStatus
+    var scope: AppScope
+    var duration: Seconds<Int>
+    var requestComment: String?
+    var responseComment: String?
+    var extraMonitoring: String?
+    var createdAt = Date()
+    var updatedAt = Date()
 
-  init(
-    id: Id = .init(),
-    computerUserId: ComputerUser.Id,
-    status: RequestStatus = .pending,
-    scope: AppScope,
-    duration: Seconds<Int> = 180,
-    requestComment: String? = nil,
-    responseComment: String? = nil,
-    extraMonitoring: String? = nil
-  ) {
-    self.id = id
-    self.computerUserId = computerUserId
-    self.status = status
-    self.scope = scope
-    self.duration = duration
-    self.requestComment = requestComment
-    self.responseComment = responseComment
-    self.extraMonitoring = extraMonitoring
+    init(
+      id: Id = .init(),
+      computerUserId: ComputerUser.Id,
+      status: RequestStatus = .pending,
+      scope: AppScope,
+      duration: Seconds<Int> = 180,
+      requestComment: String? = nil,
+      responseComment: String? = nil,
+      extraMonitoring: String? = nil
+    ) {
+      self.id = id
+      self.computerUserId = computerUserId
+      self.status = status
+      self.scope = scope
+      self.duration = duration
+      self.requestComment = requestComment
+      self.responseComment = responseComment
+      self.extraMonitoring = extraMonitoring
+    }
   }
 }
 
 // loaders
 
-extension SuspendFilterRequest {
+extension MacApp.SuspendFilterRequest {
   func userDevice(in db: any DuetSQL.Client) async throws -> ComputerUser {
     try await ComputerUser.query()
       .where(.id == self.computerUserId)

swift/api/Sources/Api/Services/AdminNotifications/AdminEvent.swift
@@ -20,12 +20,22 @@ enum AdminEvent: Equatable {
   }
 
   struct SuspendFilterRequestSubmitted: Equatable {
+    enum Context: Equatable {
+      case macapp(
+        computerUserId: ComputerUser.Id,
+        requestId: MacApp.SuspendFilterRequest.Id
+      )
+      case iosapp(
+        deviceId: IOSApp.Device.Id,
+        requestId: IOSApp.SuspendFilterRequest.Id
+      )
+    }
+
     var dashboardUrl: String
-    var userDeviceId: ComputerUser.Id
-    var userId: User.Id
-    var userName: String
+    var childId: User.Id
+    var childName: String
     var duration: Seconds<Int>
-    var requestId: SuspendFilterRequest.Id
     var requestComment: String?
+    var context: Context
   }
 }