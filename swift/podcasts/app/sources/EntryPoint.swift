22136372d11ef64fb3616038597678fc38ccdd02
Jared Henderson <jaredh159@users.noreply.github.com>
2025-07-28 15:52:50 -0400

    scaffold project with xcodegen

---
+++ swift/podcasts/app/sources/EntryPoint.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+@main
+struct EntryPoint: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+    .ignoresSafeArea()
+  }
+}
+
+#Preview {
+  ContentView()
+}
