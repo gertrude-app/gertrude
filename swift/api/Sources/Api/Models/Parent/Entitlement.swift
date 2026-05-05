da4cd3122a160deee04aba8b52fc84f2100a194f
Jared Henderson <jaredh159@users.noreply.github.com>
2026-05-05 14:08:12 -0400

    api: add billing entities

---
+++ swift/api/Sources/Api/Models/Parent/Entitlement.swift
@@ -0,0 +1,16 @@
+import Foundation
+import XCore
+
+enum Entitlement: Equatable, Sendable {
+  case free
+  case light
+  case fullTrial(until: Date)
+  case fullTrialGrace(until: Date)
+  case full
+  case complimentary
+}
+
+extension Entitlement {
+  static let trialPeriod: TimeInterval = .days(21)
+  static let gracePeriod: TimeInterval = .days(7)
+}
