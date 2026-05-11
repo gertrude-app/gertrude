84087fb41087b16271e2919afc534bd15526f0b8
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2026-05-11 10:11:32 -0400

    ui: made sidebar component

---
+++ web/ui/src/components/ui/sidebar/SidebarSubItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Props {
+  title: string;
+}
+
+const SidebarSubItem: React.FC<Props> = ({ title }) => {
+  return (
+    <div className="rounded-xl cursor-default relative group">
+      <div className="absolute left-2 top-0 h-full w-0.25 bg-stone-200 group-last:h-[80%]" />
+      <div className="py-1.25 hover:bg-stone-200/30 ml-4 px-2 rounded-xl active:bg-stone-200/50 active:scale-98 transition-[scale] duration-100">
+        <span className="text-sm text-stone-900 select-none">{title}</span>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarSubItem;
