4336b2d7ce247bb7bf43c370469cf1fd511d5720
Jared Henderson <jaredh159@users.noreply.github.com>
2026-06-29 16:37:14 -0400

    web: align core deps, bump typescript to 6.0.3

--- web/images.d.ts
+++ web/images.d.ts
@@ -1,30 +1,32 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 declare module '*.png' {
-  const content: import('next/image').StaticImageData;
+  const content: import(`next/image`).StaticImageData;
   export default content;
 }
 
 declare module '*.jpg' {
-  const content: import('next/image').StaticImageData;
+  const content: import(`next/image`).StaticImageData;
   export default content;
 }
 
 declare module '*.jpeg' {
-  const content: import('next/image').StaticImageData;
+  const content: import(`next/image`).StaticImageData;
   export default content;
 }
 
 declare module '*.gif' {
-  const content: import('next/image').StaticImageData;
+  const content: import(`next/image`).StaticImageData;
   export default content;
 }
 
 declare module '*.webp' {
-  const content: import('next/image').StaticImageData;
+  const content: import(`next/image`).StaticImageData;
   export default content;
 }
 
 declare module '*.svg' {
-  const content: import('next/image').StaticImageData;
+  const content: import(`next/image`).StaticImageData;
   export default content;
 }
+
+declare module '*.css';
