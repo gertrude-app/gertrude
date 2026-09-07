5161a1971ae3663b2e002b49c6d874e7ae4050d3
Jared Henderson <jaredh159@users.noreply.github.com>
2026-09-07 10:04:50 -0400

    account: dont logout on hover logout button

--- web/account/src/routes/_app/signout.tsx
+++ web/account/src/routes/_app/signout.tsx
@@ -2,7 +2,8 @@ import { createFileRoute, redirect } from '@tanstack/react-router';
 import { clearAuth } from '#/pairql/auth';
 
 export const Route = createFileRoute(`/_app/signout`)({
-  beforeLoad: ({ context }) => {
+  beforeLoad: ({ cause, context }) => {
+    if (cause === `preload`) return;
     clearAuth();
     context.queryClient.clear();
     throw redirect({ to: `/login` });
