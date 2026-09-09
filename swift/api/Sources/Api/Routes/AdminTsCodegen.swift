7c60da06ac1dc243ba9a7d3df214038d86476b6e
Jared Henderson <jaredh159@users.noreply.github.com>
2026-09-09 14:14:56 -0400

    api: deduplicate pairql ts codegen

--- swift/api/Sources/Api/Routes/AdminTsCodegen.swift
+++ swift/api/Sources/Api/Routes/AdminTsCodegen.swift
@@ -1,18 +1,20 @@
+import Foundation
 import Gertie
 import GertieApp
 import PairQL
 import TypeScriptInterop
-import Vapor
 
-enum AdminTsCodegenRoute {
-  struct Response: Content {
-    struct Pair: Content {
-      let pair: String
-      let fetcher: String
-    }
+enum AdminTsCodegenRoute: PairQLTsCodegenRoute {
+  static var logName: String {
+    "Admin".red
+  }
 
-    var shared: [String: String]
-    var pairs: [String: Pair]
+  static var sharedAliases: [Config.Alias] {
+    [
+      .init(NoInput.self, as: "void"),
+      .init(Date.self, as: "ISODateString"),
+      .init(StripeSubscription.StripeId.self, as: "string"),
+    ]
   }
 
   static var sharedTypes: [(String, Any.Type)] {
@@ -60,64 +62,5 @@ enum AdminTsCodegenRoute {
       GetRecentPairqlErrors.self,
       GetParentRecentTelemetry.self,
     ]
-  }
-
-  static func generate() throws -> Response {
-    var shared: [String: String] = [:]
-    var sharedAliases: [Config.Alias] = [
-      .init(NoInput.self, as: "void"),
-      .init(Date.self, as: "ISODateString"),
-      .init(StripeSubscription.StripeId.self, as: "string"),
-    ]
-    var config = Config(compact: true, aliasing: sharedAliases)
-
-    for (name, type) in self.sharedTypes {
-      shared[name] = try CodeGen(config: config).declaration(for: type, as: name)
-      sharedAliases.append(.init(type, as: name))
-      config = .init(compact: true, aliasing: sharedAliases)
-    }
-
-    var pairs: [String: Response.Pair] = [:]
-    for pairType in self.pairqlPairs {
-      pairs[pairType.name] = try self.ts(for: pairType, with: config)
-    }
-
-    return Response(shared: shared, pairs: pairs)
-  }
-
-  @Sendable static func handler(_ request: Request) async throws -> Response {
-    request.logger.notice("TS codegen: \("Admin".red)")
-    return try self.generate()
-  }
-
-  private static func ts<P: Pair>(
-    for type: P.Type,
-    with config: Config,
-  ) throws -> Response.Pair {
-    let codegen = CodeGen(config: config)
-    let name = "\(P.self)"
-    var pair = try """
-    export namespace \(name) {
-      \(codegen.declaration(for: P.Input.self, as: "Input"))
-
-      \(codegen.declaration(for: P.Output.self, as: "Output"))
-    }
-    """
-
-    let pairLines = pair.split(separator: "\n")
-    if pairLines.count == 4, pairLines.allSatisfy({ $0.count < 60 }) {
-      pair = pairLines.joined(separator: "\n")
-    }
-
-    var fetchName = "\(name)".regexReplace("_.*$", "")
-    let firstLetter = fetchName.removeFirst()
-    let functionName = String(firstLetter).lowercased() + fetchName
-
-    let fetcher = """
-    \(functionName) = (input: P.\(name).Input): Promise<Result<P.\(name).Output>> => {
-      return this.query<P.\(name).Output>(input, `\(P.name)`, `\(P.auth)`);
-    }
-    """
-    return .init(pair: pair, fetcher: fetcher)
   }
 }
